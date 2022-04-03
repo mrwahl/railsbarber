35aaaac2a3378d3d17e48932720b4c76194ed465
Mr Wahl <filipmunteanu2@gmail.com>
2022-04-03 20:20:16 +0100

    remove something

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
-    before_action :set_customer, only: %i[ show edit update destroy ]
-   #before we run anything if the user is not signed in show index and show functions
-   before_action :authenticate_user!, except: [:index,:show]
-   #only the correct user can edit,update and destroy
-   before_action :correct_user, only: [:edit, :update , :destroy]
+ 
     def home
     end
   end
